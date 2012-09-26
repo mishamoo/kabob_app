6f4c3c5a96138487d5e6657b1b408d52d80efc14
Mijail Benitez <mijailbenitez@gmail.com>
2012-09-26 11:02:26 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KabobApp::Application.config.secret_token = '818ac20e41829565c2c1a72e01c4169635331c7784409263f5507fb463a2fb9132411c63b220c663187f05df9ea27ef4c82df9a22b0f2844310f927a504dec8a'
